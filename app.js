3e77cea9519d3d08cd5bf53aa532360978576af2
SivadeepKothaKBK <sivadeepkothakbk@gmail.com>
2025-04-08 15:32:28 -0500

    Update app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const Sivadeep ={
+    "name":"Sivadeep",
+    "status": "professor",
+    "power level": 9000,
+}
+
+    const Kumar ={
+        "name":"Kumar",
+        "status": "professor",
+        "power level": 9000,
+    }  
+
+    const Bhanu ={
+        "name":"Bhanu",
+        "status": "professor",
+        "power level": 9000,
+    }
+
+    const gouse ={
+        "name":"gouse",
+        "status": "professor",
+        "power level": 9000,
+    }
+
+    const sasi ={
+        "name":"sasi",
+        "status": "professor",
+        "power level": 9000,
+    }
