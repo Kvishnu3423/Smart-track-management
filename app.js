6b23ab379b3fcbdd25e874e3b0081bd5a01b7362
vbp23 <163583633+vbp23@users.noreply.github.com>
2025-04-10 14:30:17 -0500

    update

---
+++ app.js
@@ -0,0 +1,34 @@
+const bhanu = {
+    "name": "bhanu",
+    "status": "student"
+    "powerlevel":9000
+}
+
+const bharath = {
+    "name": "bharath",
+    "status": "student"
+    "powerlevel":9000
+}
+
+const gouse = {
+    "name": "gouse",
+    "status": "student"
+    "powerlevel":9000
+}
+
+const kumar = {
+    "name": "kumar",
+    "status": "student"
+    "powerlevel":9000
+}
+
+const sivadeep = {
+    "name": "sivadeep",
+    "status": "student"
+    "powerlevel":9000
+}
+const sasi = {
+    "name": "sasi",
+    "status": "student"
+    "powerlevel":9000
+}
